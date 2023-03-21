6c5358a49938ffa3c3acffb0305788f6bd57f1cd
Luke Lai <lukelai.dev@gmail.com>
2023-03-21 23:36:37 +0000

    Add a component test for getTransactionStatement method

--- transaction.test.js
+++ transaction.test.js
@@ -14,4 +14,15 @@ describe("Transaction class", () => {
     transaction.createTransaction("13/01/2023", "credit", 100, 200);
     expect(transaction.getTransactionData()).toEqual(data);
   });
+
+  it("outputs a statement from transaction data", () => {
+    const transaction = new Transaction();
+    transaction.createTransaction("13/01/2023", "credit", 1000, 2000);
+    const [startingRecord, transactionRecord] =
+      transaction.getTransactionStatement("13/01/2023", 1000);
+    expect(startingRecord).toEqual("13/01/2023 || N/A || N/A || 1000.00\n");
+    expect(transactionRecord).toEqual(
+      "13/01/2023 || 1000.00 || N/A || 2000.00\n"
+    );
+  });
 });
