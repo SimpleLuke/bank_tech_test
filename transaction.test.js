bdd45fc6acf06e6b1f02b0413d083ee0dd85fa42
Luke Lai <lukelai.dev@gmail.com>
2023-03-21 23:27:40 +0000

    Add a component test for Transaction class createTransaction method and getTransactionData method

---
+++ transaction.test.js
@@ -0,0 +1,17 @@
+const Transaction = require("./transaction");
+
+describe("Transaction class", () => {
+  it("creates a transaction and return as an array", () => {
+    const transaction = new Transaction();
+    const data = [
+      {
+        date: "13/01/2023",
+        type: "credit",
+        amount: "100.00",
+        balance: "200.00",
+      },
+    ];
+    transaction.createTransaction("13/01/2023", "credit", 100, 200);
+    expect(transaction.GetTransactionData()).toEqual(data);
+  });
+});
